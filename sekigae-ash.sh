bd94cf0e42105fb4ca4c21c200d6f40e33d742ad
HIROSE Masaaki <hirose31@gmail.com>
2013-05-24 21:04:32 +0900

    More simple

--- sekigae-ash.sh
+++ sekigae-ash.sh
@@ -2,32 +2,12 @@
 
 newcomers="okkun keoken kitak gussan"
 
-n_newcomers=$(echo $newcomers | wc -w)
-
 random() {
   echo $(tr -dc 0-9 </dev/urandom | head -c5 | sed -e 's/^0//')
 }
 
-i=0
-newcomers=$(
 for p in $newcomers; do
-  echo -n " $i:$p"
-  i=$(($i+1))
-done
-)
-
-i=0
-while [ $i -lt $n_newcomers ]; do
-  n=$(($(random) % $n_newcomers))
-
-  tmp_n=$(echo "$newcomers" | egrep -o " $n:[^ ]+")
-  tmp_i=$(echo "$newcomers" | egrep -o " $i:[^ ]+")
-  newcomers=$(echo "$newcomers"    | \
-    sed -e "s/$tmp_n/ __SWAPME__/" | \
-    sed -e "s/$tmp_i/$tmp_n/"      | \
-    sed -e "s/ __SWAPME__/$tmp_i/" )
-
-  i=$(($i+1))
-done
-
-echo $(echo "$newcomers" | sed -e "s/ [0-9]:/ /g")
+  r=$(random)
+  echo "$r $p"
+done | sort -k1n | cut -d' ' -f2 | tr '\n' ' '
+echo
