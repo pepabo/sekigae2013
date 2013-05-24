2bba921435970200dcc8799c150a0c62d78f2dff
aka. bakaiku or @gdgdiler <tko@jitu.org>
2013-05-25 00:59:48 +0900

    "|"でつなぐのめんどいし、他の言語でもやってないのもあるからやんない。

--- Sekigae.java
+++ Sekigae.java
@@ -1,19 +1,9 @@
-import java.util.ArrayList;
-import java.util.Collections;
- 
-public class Sekigae{
-    public static void main(String[] args){
- 
-  ArrayList<String> list = new ArrayList<String>();
- 
-	list.add("okkun");
-	list.add("kitak");
-	list.add("keoken");
-	list.add("gussan");
- 
-	Collections.shuffle(list);
- 
-	System.out.println(list.get(0) +" | "+ list.get(1) +" | "+ 
-			   list.get(2) +" | "+ list.get(3)); 
+import java.util.*;
+
+public class Sekigae {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("たけお", "きたけー", "ぐっさん", "おっくん");
+        Collections.shuffle(list);
+        System.out.println(Arrays.toString(list.toArray()));
     }
 }
