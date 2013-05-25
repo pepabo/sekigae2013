76dde912306edecb28d5a56aa01cbf3e7b2c6632
adarapata <efb.youdie@gmail.com>
2013-05-25 16:43:53 +0900

    fix insert max length

--- unity_sekigae_script/SekigaeLogic.cs
+++ unity_sekigae_script/SekigaeLogic.cs
@@ -5,7 +5,11 @@ public class SekigaeLogic {
     static public ArrayList Sekigsae(ArrayList array)
     {
         ArrayList newArray = new ArrayList(array.Count);
-        foreach (var n in array) { newArray.Insert(Random.Range(0, newArray.Count), n); }
+        foreach (var n in array) 
+        {
+            var index = Random.Range(0, newArray.Count+1);
+            newArray.Insert(index, n);
+        }
         return newArray;
     }
 }
