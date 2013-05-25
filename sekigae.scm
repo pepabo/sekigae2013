61a1a6132b587e83990c1920e685fce283001f44
Keisuke KITA <kei.kita2501@gmail.com>
2013-05-25 15:58:15 +0900

    Add: scheme version

---
+++ sekigae.scm
@@ -0,0 +1,13 @@
+(use srfi-27)
+
+(random-source-randomize! default-random-source)
+(define freshers (list "okkun" "gussan" "keoken" "takeo"))
+
+(print
+  (string-join
+    (map  
+      (lambda (x) 
+        (car (cdr x)))
+      (sort (map (lambda (x)
+              (list (random-real) x)) freshers)
+              (lambda (x y) (< (car x) (car y))))) " | "))
